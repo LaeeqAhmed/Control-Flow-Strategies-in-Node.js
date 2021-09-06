de5ea1668e5dab9828c5c63596f31abe52d0841a
Laeeq Ahmed <laeeqahmed0300@gmail.com>
2021-09-07 02:29:19 +0500

    server task-01 node.js complete

--- server/index.js
+++ server/index.js
@@ -1,27 +1,63 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+import cheerio  from 'cheerio';
 const app = express();
 const router = express.Router();
 
-app.get('/I/want/title/', function(req, res) {
+app.get('/I/want/title/', async function(req, res) {
     const { address } = req.query;
+
     //console.log("address is "+ address);
-    let previousText="<html>\n<head><\/head>\n<body>\n<h1> Following are the titles of given websites: <\/h1><ul>";
-	let text="";
-	let arrayOfString = address.toString().split(",");
-
-    for (let i = 0; i < arrayOfString.length; i++)
+    var previousText="<html>\n<head><\/head>\n<body>\n<h1> Following are the titles of given websites: <\/h1><ul>";
+	res.write(previousText);
+    var text_li="";
+	var text="";
+    var title="";
+	var arrayOfString = address.toString().split(",");
+     
+    console.log(arrayOfString)
+    function validateUrl(value)
     {
-        text = text + "<li>"+ arrayOfString[i] + "<\/li>\n";
+        console.log("value"+value);
+        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
+        var regexp = new RegExp(expression);
+        return regexp.test(value);
+    } 
+    for (var i = 0; i < arrayOfString.length; i++)
+    {
+        console.log("expression output:"+validateUrl(arrayOfString[i]));
+        console.log(arrayOfString[i]);
+		text_li="";
+        if(validateUrl(arrayOfString[i])){
+            const x = await fetch("https://"+arrayOfString[i])
+            .then(res => res.text())
+            .then((text) => {
+                var $ = cheerio.load(text);
+                title = $("title").text();
+                text_li= "<li>"+ arrayOfString[i]+" - "+ title +"<\/li>\n";
+                res.write(text_li);
+                console.log(arrayOfString[i]);
+                console.log("title:"+title);
+                return title;
+            });
+            console.log("x:"+x.toString());
+    }else{
+        res.write(arrayOfString[i]+" - NO RESPONSE");
     }
-		
-text=previousText+text;
-let lastText="<\/ul>\n<\/body>\n<\/html>";
-text=text+lastText;
+    }
 
-res.send(text);
+var lastText="<\/ul>\n<\/body>\n<\/html>";
+res.write(lastText);
+res.end();
 
 });
+
+//error 404
+app.get('*', function (req, res) {
+
+    res.status(404).json({ Message: 'Not Found' });
+    
+})
 
 app.listen(3000);
 
